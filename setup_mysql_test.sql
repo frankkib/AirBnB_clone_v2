efb09543637e7d2a98929e0da78627a1a4ef9e91
frankkib <frankkibagendi13@gmail.com>
2023-04-22 17:46:50 +0000

    a script that prepares a MySQL server for the project:

---
+++ setup_mysql_test.sql
@@ -0,0 +1,6 @@
+-- A script that creates database user and grants privileges
+CREATE DATABASE IF NOT EXISTS hbnb_test_db;
+-- Creating user
+CREATE USER IF NOT EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_test'@'localhost';
+GRANT SELECT ON performance_schema .* TO 'hbnb_test'@'localhost';
