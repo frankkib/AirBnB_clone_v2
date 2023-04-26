656494efc7e9e4cf0a1507b066e39ce9256a539e
frankkib <frankkibagendi13@gmail.com>
2023-04-26 11:33:14 +0000

    upadating the file object thta deletes all objects

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,9 +8,11 @@ class FileStorage:
     __file_path = 'file.json'
     __objects = {}
 
-    def all(self):
+    def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
-        return FileStorage.__objects
+        if cls is None:
+            return self.objects
+        return {k: v for k, v in self.__objects.items() if isinstance(v, cls)
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -24,6 +26,13 @@ class FileStorage:
             for key, val in temp.items():
                 temp[key] = val.to_dict()
             json.dump(temp, f)
+    def delete(self, obj=None):
+        """Deletes all the objects"""
+        if obj is not None:
+            key = obj.__class__.name__ + '.' + obj.id
+            if key in self.__objects:
+                del self.__objects[key]
+            self.save()
 
     def reload(self):
         """Loads storage dictionary from file"""
