9a1027218148a4f5d418d635d9a6b6af9490c7bf
frankkib <frankkibagendi13@gmail.com>
2023-05-04 19:26:35 +0000

    delopying the archives to the web servers

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,15 +5,17 @@ from os.path import exists
 
 env.hosts = ['174.129.68.202', '100.26.53.81']
 
+
 def do_deploy(archive_path):
     """deploying archives to web servers"""
     if not exists(archive_path):
         return False
-    filename  = archive_path,split("/")[-1]
+    filename = archive_path.split("/")[-1]
     no_extra = filename.split(".")[0]
     put(archive_path, "/tmp")
     run("mkdir -p /data/web_static/releases/{}".format(no_extra))
-    run("tar -xzf /tmp/{} -C /data/web_static/releases/".format(filename, no_extra))
+    run("tar -xzf /tmp/{} -C \
+            /data/web_static/releases/".format(filename, no_extra))
     run("rm /tmp/{}".format(filename))
     run("mv /data/web_static/releases/{}/web_static/*\
             /data/web_static/releases/{}".format(no_extra, no_extra))
