07aec422ce6989556aa95990627f86c692fa6f41
frankkib <frankkibagendi13@gmail.com>
2023-05-18 09:46:27 +0000

    A script that starts a web application

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""python module that starts a web flask app"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    return 'Hello HBNB'
+
+
+if __name__ == "__main__":
+    app.run(host='127.0.0.1', port=5000)
