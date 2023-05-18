34e927c06f5fd8948be697d85d9946fef66d9ace
frankkib <frankkibagendi13@gmail.com>
2023-05-18 11:02:43 +0000

    a script that starts a Flask web application:

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+"""python module that starts a web flask app"""
+from flask import Flask, escape
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """returns hello HBNB"""
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """returns HBNB path"""
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def some_text():
+    """returns sometext passed in"""
+    text = escape(text.replace('_', ' '))
+    return f"C {text}"
+
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def show_python():
+    """shows the sub_path"""
+    text = escape(text.replace('_', ' '))
+    return f"Python{text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """return number of times"""
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
